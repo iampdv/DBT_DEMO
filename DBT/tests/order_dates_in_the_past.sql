d77c83443202d6b5e1925c80d9794acabf36be2c
iampdv <martin.potdevin@gmail.com>
2025-07-01 14:41:10 +0200

    Revert "Refactor dbt project structure and add staging models for customers and orders"
    This reverts commit 2378c72f5713a011040d1c025d2d430879f5ecc0.

--- DBT/tests/order_dates_in_the_past.sql
+++
@@ -1,8 +0,0 @@
-with invalid_orders as (
-    select *
-    from {{ ref('stg_orders') }}
-    where ordered_at > current_date
-)
-select count(*)
-from invalid_orders
-having count(*) > 0
--- DBT/tests/relation_test_customer.sql
+++
@@ -1,5 +0,0 @@
-select o.customer_id , c.customer_id
-from {{ ref('stg_orders') }} o
-join {{ ref('stg_customers') }} c
-on o.customer_id = c.customer_id
-where o.customer_id = c.customer_id;
